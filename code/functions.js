1214545da8aff53fa7a2981076538c0ec7928691
LipezJ <juandavidlipez47@gmail.com>
2022-12-19 21:42:45 -0500

    serve

---
+++ code/functions.js
@@ -0,0 +1,32 @@
+function encriptar() {
+    let keys = {
+        a: "ai",
+        e: "enter",
+        i: "imes",
+        o: "ober",
+        u: "ufat",
+    };
+    let word = document.querySelector("#input").value;
+    document.querySelector("#copiar-area").innerHTML = word
+        .split("")
+        .map((letter) => {
+            if (Object.keys(keys).indexOf(letter) >= 0) {
+                return keys[letter];
+            }
+            return letter;
+        })
+        .join("");
+}
+
+function desencriptar() {
+    let keys = { a: 2, e: 5, i: 4, o: 4, u: 4 };
+    let i = 0;
+    let result = "";
+    let word = document.querySelector("#input").value;
+    while (i < word.length) {
+        result += word[i];
+        if (Object.keys(keys).indexOf(word[i]) < 0) i++;
+        else i += keys[word[i]];
+    }
+    document.querySelector("#copiar-area").innerHTML = result;
+}
