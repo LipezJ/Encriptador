232a936848902456c05b99aac97203483e7e8ca1
LipezJ <juandavidlipez47@gmail.com>
2022-12-22 17:38:59 -0500

    minusculas

--- code/functions.js
+++ code/functions.js
@@ -1,3 +1,4 @@
+const re = /^[a-z\s]+$/;
 function encriptar() {
     const keys = {
         a: "ai",
@@ -7,43 +8,50 @@ function encriptar() {
         u: "ufat",
     };
     let word = document.querySelector("#input").value;
-    if (word.length >= 1)
-    document.querySelector("#copiar-area").value = word
-        .split("")
-        .map((letter) => {
-            if (Object.keys(keys).indexOf(letter) >= 0) return keys[letter];
-            return letter;
-        })
-        .join("");
-}
-
-function desencriptar() {
-    const keys = {'ai' : 'a', 'enter': 'e', 'imes': 'i', 'ober': 'o', 'ufat': 'u' };
-    let i = true;
-    let word = document.querySelector("#input").value;
-    if (word.length >= 1){
-        while(i){
-            i = false
-            Object.keys(keys).forEach((letter) => {
-                if (word.indexOf(letter) >= 0) {
-                    word = word.replace(letter, keys[letter])
-                    i = true
-                }
-            })
-        }
+    if (word.length >= 1 && word.match(re))
         document.querySelector("#copiar-area").value = word
+            .split("")
+            .map((letter) => {
+                if (Object.keys(keys).indexOf(letter) >= 0) return keys[letter];
+                return letter;
+            })
+            .join("");
+    else if (!word.match(re)) {
+        alert("solo se permiten minusculas");
     }
 }
 
-function copiar() {   
-    navigator.permissions.query({name: "clipboard-write"}).then((result) => {
-        if (result.state === "granted" || result.state === "prompt") {
-            navigator.clipboard.writeText(document.querySelector('#copiar-area').value)
+function desencriptar() {
+    const keys = { ai: "a", enter: "e", imes: "i", ober: "o", ufat: "u" };
+    let i = true;
+    let word = document.querySelector("#input").value;
+    if (word.length >= 1 && word.match(re)) {
+        while (i) {
+            i = false;
+            Object.keys(keys).forEach((letter) => {
+                if (word.indexOf(letter) >= 0) {
+                    word = word.replace(letter, keys[letter]);
+                    i = true;
+                }
+            });
         }
-      });
+        document.querySelector("#copiar-area").value = word;
+    } else if (!word.match(re)) {
+        alert("solo se permiten minusculas");
+    }
+}
+
+function copiar() {
+    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+        if (result.state === "granted" || result.state === "prompt") {
+            navigator.clipboard.writeText(
+                document.querySelector("#copiar-area").value
+            );
+        }
+    });
 }
 
 function limpiar() {
-    document.querySelector('#input').value = ''
-    document.querySelector('#copiar-area').value = ''
+    document.querySelector("#input").value = "";
+    document.querySelector("#copiar-area").value = "";
 }
