645a5c9bbb6369a85b1f2c3bb1e39beac3e911e5
LipezJ <juandavidlipez47@gmail.com>
2022-12-22 22:31:19 -0500

    alert lowercase

--- code/functions.js
+++ code/functions.js
@@ -1,5 +1,5 @@
 const re = /^[a-z\s]+$/;
-function encriptar() {
+function encriptar(e) {
     const keys = {
         a: "ai",
         e: "enter",
@@ -16,12 +16,12 @@ function encriptar() {
                 return letter;
             })
             .join("");
-    else if (!word.match(re)) {
-        alert("solo se permiten minusculas");
+    else if (!word.match(re) && word.length >= 1) {
+        soloMinus(e)
     }
 }
 
-function desencriptar() {
+function desencriptar(e) {
     const keys = { ai: "a", enter: "e", imes: "i", ober: "o", ufat: "u" };
     let i = true;
     let word = document.querySelector("#input").value;
@@ -36,8 +36,8 @@ function desencriptar() {
             });
         }
         document.querySelector("#copiar-area").value = word;
-    } else if (!word.match(re)) {
-        alert("solo se permiten minusculas");
+    } else if (!word.match(re) && word.length >= 1) {
+        soloMinus(e)
     }
 }
 
@@ -55,3 +55,22 @@ function limpiar() {
     document.querySelector("#input").value = "";
     document.querySelector("#copiar-area").value = "";
 }
+
+function soloMinus(e) {
+    let alert_ = document.querySelector('#alert')
+    e.target.className = 'animate__animated animate__headShake'
+    setTimeout(() => e.target.className = '', 1000)
+    alert_.style.display = 'block'
+    alert_.className = 'animate__animated animate__headShake'
+}
+function lasSolo() {
+    let alert_ = document.querySelector('#alert')
+    alert_.className = ''
+    alert_.style.display = 'none'
+}
+
+document.querySelector('#limpiarb').addEventListener('click',e => limpiar())
+document.querySelector('#encripb').addEventListener('click',e => encriptar(e))
+document.querySelector('#desencripb').addEventListener('click',e => desencriptar(e))
+document.querySelector('#copiarb').addEventListener('click', e => copiar())
+document.querySelector('#input').addEventListener('focus', e => lasSolo())
